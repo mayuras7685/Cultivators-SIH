f4e1ef715ce7de317bc5ff00fdcf6779a4da58ba
mayuras7685 <mayurasodara@gmail.com>
2022-08-21 21:30:44 +0530

    updated routes

--- app.py
+++ app.py
@@ -13,6 +13,22 @@ def home():
   title = 'Kruषक - Home'
   return render_template('index.html', title=title)
 
+# render crop recommendation form page
+
+
+@ app.route('/crop-recommend')
+def crop_recommend():
+    title = 'Kruषक - Crop Recommendation'
+    return render_template('crop.html', title=title)
+
+# render fertilizer recommendation form page
+
+
+@ app.route('/fertilizer')
+def fertilizer_recommendation():
+    title = 'Kruषक - Fertilizer Suggestion'
+
+    return render_template('fertilizer.html', title=title)
 # ===============================================================================================
 
 if __name__ == '__main__':
